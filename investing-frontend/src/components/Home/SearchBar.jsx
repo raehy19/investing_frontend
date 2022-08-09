2d39b2b13618ed5b4c4e9dd37c0c06e0a5e6c083
Raehyeon Jeong <raehy19@gmail.com>
2022-08-09 16:33:54 +0900

    feat : add searchbar routing method => route to stock chart

--- investing-frontend/src/components/Home/SearchBar.jsx
+++ investing-frontend/src/components/Home/SearchBar.jsx
@@ -1,12 +1,40 @@
 import styled from 'styled-components';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 function SearchBar() {
-	return <Container type="search" placeholder="검색" />;
+	const [searchKeyword, setSearchKeyword] = useState('');
+	const router = useRouter();
+
+	return (
+		<Container>
+			<Input
+				type="search"
+				placeholder="검색"
+				value={searchKeyword}
+				onChange={(e) => {
+					return setSearchKeyword(e.target.value);
+				}}
+			/>
+			<Button
+				type="button"
+				onClick={() => {
+					return router.push(`/chart/${searchKeyword}`);
+				}}
+			>
+				검색
+			</Button>
+		</Container>
+	);
 }
 
 export default SearchBar;
 
-const Container = styled.input`
+const Container = styled.div`
+	display: flex;
+`;
+
+const Input = styled.input`
 	padding: 1rem;
 	display: flex;
 	justify-content: space-between;
@@ -16,4 +44,8 @@ const Container = styled.input`
 	max-width: 95%;
 	border-radius: 12px;
 	box-shadow: 0 1px 8px rgba(0, 0, 0, 0.25);
+`;
+
+const Button = styled.button`
+	padding: 8px;
 `;
