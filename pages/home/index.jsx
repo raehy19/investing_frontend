ac69da0b4f9673952ed11cb2ca3cc183838f51f2
Raehyeon Jeong <raehy19@gmail.com>
2022-08-30 02:44:19 +0900

    fix : fix styled component, duplicated file(SearchBar)

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,4 +1,4 @@
-import SearchBar from '../../src/components/SearchBar';
+import SearchBar from '../../src/components/SearchBar.jsx';
 import Stocks from '../../src/components/Stocks/Stocks';
 import styled from 'styled-components';
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,16 @@
 import styled from 'styled-components';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 
-function SearchBar() {
-	const [searchKeyword, setSearchKeyword] = useState('');
+function SearchBar(props) {
+	const stocks = props.stockItems;
+	const [search, setSearch] = useState('');
+	const filteredStocks = stocks.filter((stock) => {
+		console.log(stock['name'].includes(search));
+		return stock['name'].includes(search);
+	});
+	const stockDisplay = search.length > 0;
+
 	const router = useRouter();
 
 	return (
@@ -11,19 +18,30 @@ function SearchBar() {
 			<Input
 				type="search"
 				placeholder="검색"
-				value={searchKeyword}
+				value={search}
 				onChange={(e) => {
-					return setSearchKeyword(e.target.value);
+					return setSearch(e.target.value);
 				}}
 			/>
 			<Button
 				type="button"
 				onClick={() => {
-					return router.push(`/chart/${searchKeyword}`);
+					return router.push(`/chart/${search}`);
 				}}
 			>
 				검색
 			</Button>
+			{stockDisplay && (
+				<ul>
+					{filteredStocks.map((stock) => {
+						return (
+							<li key={stock.name}>
+								{stock.name} {stock.change}
+							</li>
+						);
+					})}
+				</ul>
+			)}
 		</Container>
 	);
 }
@@ -32,6 +50,10 @@ export default SearchBar;
 
 const Container = styled.div`
 	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	height: 60px;
+	margin: 10px 0px;
 `;
 
 const Input = styled.input`
@@ -40,12 +62,15 @@ const Input = styled.input`
 	justify-content: space-between;
 	align-items: center;
 	margin: 2rem auto;
-	width: 50rem;
-	max-width: 95%;
+	width: 45rem;
+	max-width: 85%;
 	border-radius: 12px;
 	box-shadow: 0 1px 8px rgba(0, 0, 0, 0.25);
 `;
 
 const Button = styled.button`
-	padding: 8px;
+	padding: 1rem;
+	width: 60px;
+	border: 2px solid black;
+	border-radius: 10px;
 `;
